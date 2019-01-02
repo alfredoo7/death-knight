277122032c10f4c40efac56e08fc411eaee2bfd5
feng.zhou <alfred0223@126.com>
2019-01-02 18:52:17 +0800

    Update

--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -1,12 +1,32 @@
-import styles from './index.css';
+// import styles from './index.css';
 
-function BasicLayout(props) {
+// function BasicLayout(props) {
+//   return (
+//     <div className={styles.normal}>
+//       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+//       { props.children }
+//     </div>
+//   );
+// }
+
+// export default BasicLayout;
+
+import React from 'react';
+import styles from './index.css';
+import Header from './Header';
+import withRouter from 'umi/withRouter';
+
+function Layout({ children, location }) {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      { props.children }
+      <Header location={location} />
+      <div className={styles.content}>
+        <div className={styles.main}>
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default BasicLayout;
+export default withRouter(Layout);
